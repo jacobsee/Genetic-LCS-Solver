ba981c37aea3b1143083b4f84f2b8aaf5cb849dc
Jacob See <jacob.d.see@gmail.com>
2017-02-18 14:40:00 -0900

    Completely misunderstood the problem. Yay!

--- Candidate.java
+++ Candidate.java
@@ -1,14 +1,28 @@
+/*
+ *
+ * Project 1
+ * Evolutionary Computing / UAA / Spring 2017
+ * Dr. Moore
+ *
+ * Author     : Jacob See (jdsee@alaska.edu)
+ *						: Neil Reutov (nreutov3@alaska.edu)
+ *
+ */
+
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.*;
 
 public class Candidate{
 
   public String bitstring = "";
   public int fitness = 0;
+  public int stringLength = 0;
   public double rouletteSlice = 0;
+  public boolean isValid = true;
 
   public Candidate(int length){
     for(int i = 0; i < length; i++){
-      bitstring += Integer.toString(ThreadLocalRandom.current().nextInt(0, 2));
+      bitstring += Integer.toString(ThreadLocalRandom.current().nextInt(0, 11)/10);
     }
   }
 
@@ -26,18 +40,52 @@ public class Candidate{
     return c;
   }
 
+  // Determine if this candidate is actually a valid subsequence.
+
+  public boolean isSubsequence(String longerString, String potentialSubsequence) {
+    StringBuilder s = new StringBuilder();
+    s.append(".*");
+    for (int i = 0; i < potentialSubsequence.length(); i++) {
+        s.append(potentialSubsequence.charAt(i));
+        s.append(".*");
+    }
+    return longerString.matches(s.toString());
+  }
+
+  // Return the string representation of this candidate's bitstring.
+
+  public String toString(String shorterString){
+    String LCS = "";
+    for(int i = 0; i < bitstring.length(); i++){
+      if(bitstring.charAt(i) == '1'){
+        LCS += shorterString.charAt(i);
+      }
+    }
+    return LCS;
+  }
+
+  // Set the fitness value for this candidate.
+
   public void calculateFitness(String stringA, String stringB){
-    int length = Math.min(stringA.length(), stringB.length());
-    //The "perfect" candidate has a fitness equal to this length.
-    for(int i = 0; i < length; i++){
-      if(bitstring.charAt(i) == '0'){
-        if(stringA.charAt(i) != stringB.charAt(i)){
-          fitness++;
-        }
+
+    String shorterString = stringA.length() <= stringB.length() ? stringA : stringB;
+    String longerString = stringA.length() > stringB.length() ? stringA : stringB;
+
+    fitness = bitstring.length();
+
+    if(!isSubsequence(longerString, toString(shorterString))){
+      isValid = false;
+    }else{
+      fitness += bitstring.length();
+    }
+
+    for(int i = 0; i < bitstring.length(); i++){
+
+      if(bitstring.charAt(i) == '1'){
+        fitness += isValid ? 2 : -0.1;
+        stringLength++;
       }else{
-        if(stringA.charAt(i) == stringB.charAt(i)){
-          fitness++;
-        }
+        fitness += isValid ? -0.1 : 0.1;
       }
     }
   }
--- LCSSolver.java
+++ LCSSolver.java
@@ -1,188 +1,254 @@
+/*
+ *
+ * Project 1
+ * Evolutionary Computing / UAA / Spring 2017
+ * Dr. Moore
+ *
+ * Author     : Jacob See (jdsee@alaska.edu)
+ *						: Neil Reutov (nreutov3@alaska.edu)
+ *
+ */
+
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class LCSSolver{
 
+	public static String testStringA, testStringB;
+	public static char choice;
+
+	// Interactive initialization.
+
+	public static void changeString(){
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("To start, there are 5 options to choose from:\n" +
+											 "1. Easy\n" +
+											 "2. Medium\n" +
+											 "3. Hard\n" +
+											 "4. Custom\n" +
+											 "5. Quit");
+		choice = keyboard.nextLine().charAt(0);
+		if(choice == '1'){
+			testStringA = "providence";
+			testStringB = "thepresident";
+		}
+		else if(choice == '2'){
+			testStringA = "mgkzpbjxwhpnvtvzxmxytohapruqhhgqbqmvvflq";
+    	testStringB = "hgvvltybqxyfipxynlhvrcszkifxzfqrjbbkiyfg";
+		}
+		else if(choice == '3'){
+			testStringA = "fjptopgabhtoopoahvkzzlnncuauwgcegqvxcxzexeyjjyhrixamprqutckr";
+    	testStringB = "oqsyzcbqchfypfhsqlkbagvmpinhzoubtlbulsvwkwivuetyyaefajuajvbp";
+		}
+		else if(choice == '4'){
+			System.out.println("Please enter string one:\n");
+			testStringA = keyboard.nextLine();
+			System.out.println("Please enter string two:\n");
+			testStringB = keyboard.nextLine();
+		}
+		else if(choice != '5'){
+			System.out.println("Error, chosen choice not listed!");
+		}
+		if(choice == '5'){
+			keyboard.close();
+			System.exit(0);
+	  }
+	}
+
+	// Use a different algorithm to compute a stopping point for the GA.
+	// Adapted from https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
+
+  public static int ExpectedLCSLength(String stringA, String stringB, int m, int n){
+    int[][] c = new int[stringA.length()+1][stringB.length()+1];
+    for(int i = 0; i <= stringA.length(); i++){
+      c[i][0] = 0;
+    }
+    for(int j = 0; j <= stringB.length(); j++){
+      c[0][j] = 0;
+    }
+    for(int i = 1; i <= stringA.length(); i++){
+      for(int j = 1; j <= stringB.length(); j++){
+        if(stringA.charAt(i-1) == stringB.charAt(j-1)){
+          c[i][j] = c[i-1][j-1] + 1;
+        }else{
+          c[i][j] = Math.max(c[i][j-1], c[i-1][j]);
+        }
+      }
+    }
+    return c[stringA.length()][stringB.length()];
+  }
+
   public static void main(String args[]){
-
     //Set up our two strings from which to evolve the LCS.
-    //Todo: Add interactivity
+		do{
+			changeString();
+	    //Create and initialize a random population of candidates
 
-    //String testStringA = "providence";
-    //String testStringB = "president"; //Correct bitstring is 110011110 with a fitness equalling the length of 9
+	    int bitLength = Math.min(testStringA.length(), testStringB.length());
+	    int populationSize = (int)Math.pow(bitLength, 2);
+	    if(populationSize > 5000){
+	        populationSize = 5000;
+	    }
+			populationSize = 1000;
 
-    String testStringA = "aawocPvsrclzstcsreszetcazcsczsecszeGcseiscvhxdtehxrgUzfgvsrAsdvnvAvbm";
-    String testStringB = "certtPzfnnlewytweebnjadadnsqweembkiGygyiiovqweyebvzaUbrsasfAawrnbAqwe";
+	    double candidateMutationProbability = 0.05;
+	    double bitMutationRate = 0.5;
+      int expectedLength = ExpectedLCSLength(testStringA, testStringB, testStringA.length(), testStringB.length());
 
-    //String testStringA = "aawocPvsrclzstcsreszetcazcsczsecszeGcseiscvhxdtehxrgUzfgvsrAsdvnvAvbmaawocPvsrclzstcsreszetcazcsczsecszeGcseiscvhxdtehxrgUzfgvsrAsdvnvAvbmaawocPvsrclzstcsreszetcazcsczsecszeGcseiscvhxdtehxrgUzfgvsrAsdvnvAvbmaawocPvsrclzstcsreszetcazcsczsecszeGcseiscvhxdtehxrgUzfgvsrAsdvnvAvbmaawocPvsrclzstcsreszetcazcsczsecszeGcseiscvhxdtehxrgUzfgvsrAsdvnvAvbm";
-    //String testStringB = "certtPzfnnlewytweebnjadadnsqweembkiGygyiiovqweyebvzaUbrsasfAawrnbAqwecerttPzfnnlewytweebnjadadnsqweembkiGygyiiovqweyebvzaUbrsasfAawrnbAqwecerttPzfnnlewytweebnjadadnsqweembkiGygyiiovqweyebvzaUbrsasfAawrnbAqwecerttPzfnnlewytweebnjadadnsqweembkiGygyiiovqweyebvzaUbrsasfAawrnbAqwecerttPzfnnlewytweebnjadadnsqweembkiGygyiiovqweyebvzaUbrsasfAawrnbAqwe";
+	    ArrayList<Candidate> population = new ArrayList<Candidate>();
 
-    //String testStringA = "supercalifragilisticexpialidocious";
-    //String testStringB = "spoercalifalfekleficexalfnkfdlioadf";
+	    for(int i = 0; i < populationSize; i++){
+	      population.add(new Candidate(bitLength));
+	    }
 
-    //Create and initialize a random population of candidates
+	    System.out.println("\nCreated a population with " + populationSize + " members.");
+      System.out.println("We are targeting a length of " + expectedLength + ".");
 
-    int bitLength = Math.min(testStringA.length(), testStringB.length());
-    int populationSize = (int)Math.pow(bitLength, 2);
-    if(populationSize > 5000){
-        populationSize = 5000;
-    }
+	    System.out.println("\nGeneration\t|\tAvg. Fitness\t|\tHigh Fitness\t|\tProgress");
+	    System.out.println("-----------------------------------------------------------");
 
-    double candidateMutationProbability = 0.25;
-    double bitMutationRate = 0.25;
+	    // Begin evolution
 
-    ArrayList<Candidate> population = new ArrayList<Candidate>();
+	    boolean solved = false;
+	    Candidate perfectCandidate = null;
+	    int generation = 0;
+	    ArrayList<Double> generationHistory = new ArrayList<Double>();
 
-    for(int i = 0; i < populationSize; i++){
-      population.add(new Candidate(bitLength));
-    }
+	    try( PrintWriter out = new PrintWriter("results.csv") ){
 
-    System.out.println("\nCreated a population with " + populationSize + " members.");
-    System.out.println("We are targeting a fitness of " + bitLength + ".");
+	      while(!solved){
 
-    System.out.println("\nGeneration\t|\tAvg. Fitness\t|\tHigh Fitness");
-    System.out.println("-----------------------------------------------------------");
+	        generation++;
 
-    // THE GOOD STUFF
+	        int fitnessSum = 0;
+	        double probabilitySum = 0;
+	        int highestFitness = 0;
+					int longestValidLength = 0;
 
-    boolean solved = false;
-    Candidate perfectCandidate = null;
-    int generation = 0;
-    ArrayList<Double> generationHistory = new ArrayList<Double>();
+	        // Need to compute all fitnesses and the sum of all fitnesses for roulette selection. Also check for the perfect candidate.
 
-    try( PrintWriter out = new PrintWriter("results.csv") ){
+	        for(int i = 0; i < populationSize; i++){
+	          population.get(i).calculateFitness(testStringA, testStringB);
+	          fitnessSum += population.get(i).fitness;
+	          if(population.get(i).fitness > highestFitness){
+	            highestFitness = population.get(i).fitness;
+	          }
+	          if(population.get(i).isValid){
+							if(population.get(i).stringLength == expectedLength){
+		            perfectCandidate = population.get(i);
+								longestValidLength = perfectCandidate.stringLength;
+		            solved = true;
+							}else{
+								longestValidLength = population.get(i).stringLength;
+							}
+	          }
+	        }
 
-      while(!solved){
+	        System.out.println(generation + "\t\t|\t" + String.format("%.2f", (float)fitnessSum / populationSize) + " \t\t|\t" + highestFitness + "\t|\t" + longestValidLength + "/" + expectedLength + "  (" + ((float)longestValidLength/expectedLength)*100 + "% Evolved)");
 
-        generation++;
+	        out.println(generation + "," + ((float)fitnessSum / populationSize) + "," + highestFitness);
 
-        int fitnessSum = 0;
-        double probabilitySum = 0;
-        int highestFitness = 0;
+	        generationHistory.add((double)fitnessSum / populationSize);
 
-        // Need to compute all fitnesses and the sum of all fitnesses for roulette selection. Also check for the perfect candidate.
+	        if(!solved){
 
-        for(int i = 0; i < populationSize; i++){
-          population.get(i).calculateFitness(testStringA, testStringB);
-          fitnessSum += population.get(i).fitness;
-          if(population.get(i).fitness > highestFitness){
-            highestFitness = population.get(i).fitness;
-          }
-          if(population.get(i).fitness == bitLength){
-            perfectCandidate = population.get(i);
-            solved = true;
-          }
-        }
+	          // No perfect candidate... We'll need to generate a new population.
 
-        System.out.println(generation + "\t\t|\t" + String.format("%.2f", (float)fitnessSum / populationSize) + " \t\t|\t" + highestFitness);
+	          // Set the population up for roulette selection.
 
-        out.println(generation + "," + ((float)fitnessSum / populationSize) + "," + highestFitness);
+	          ArrayList<Candidate> temp = new ArrayList<Candidate>();
 
-        generationHistory.add((double)fitnessSum / populationSize);
+	          for(int i = 0; i < populationSize; i++){
+	            temp.add(population.get(i).clone());
+	            temp.get(i).rouletteSlice = probabilitySum + ((double)temp.get(i).fitness / fitnessSum);
+	            probabilitySum = temp.get(i).rouletteSlice;
+	          }
 
-        if(!solved){
+	          population.clear();
 
-          // No perfect candidate... We'll need to generate a new population.
+						ArrayList<String> usedBitstrings = new ArrayList<String>();
 
-          // Set the population up for roulette selection.
+	          // Begin spawning next generation of candidates
 
-          ArrayList<Candidate> temp = new ArrayList<Candidate>();
+	          while(population.size() < populationSize){
 
-          for(int i = 0; i < populationSize; i++){
-            temp.add(population.get(i).clone());
-            temp.get(i).rouletteSlice = probabilitySum + ((double)temp.get(i).fitness / fitnessSum);
-            probabilitySum = temp.get(i).rouletteSlice;
-          }
+	              // Begin roulette selection
 
-          population.clear();
+	              Candidate selectedFirst = null;
+	              Candidate selectedSecond = null;
+	              while(selectedFirst == null || selectedSecond == null){
+	                double rouletteBall = ThreadLocalRandom.current().nextDouble(1);
+	                for(int i = 0; i < populationSize; i++){
+	                  if((i > 0 && (rouletteBall < temp.get(i).rouletteSlice && rouletteBall > temp.get(i - 1).rouletteSlice)) || (i < 0 && (rouletteBall < temp.get(i).rouletteSlice))) {
+	                    if(selectedFirst == null){
+	                      selectedFirst = temp.get(i);
+	                    }else{
+	                      selectedSecond = temp.get(i);
+	                    }
+	                  }
+	                }
+	              }
 
-          // Begin spawning next generation of candidates
+	              if(ThreadLocalRandom.current().nextDouble(1) > candidateMutationProbability){
 
-          while(population.size() < populationSize){
+	                // Begin crossover of selected candidates to create child candidate
 
-              // Begin roulette selection
+	                int slicePoint = ThreadLocalRandom.current().nextInt(1, bitLength); //Crossover at least 1 char at the very beginning or very end, never before or after the entire bitstring
+	                String newBitstring1 = selectedFirst.bitstring.substring(0, slicePoint) + selectedSecond.bitstring.substring(slicePoint);
+									String newBitstring2 = selectedSecond.bitstring.substring(0, slicePoint) + selectedFirst.bitstring.substring(slicePoint);
 
-              Candidate selectedFirst = null;
-              Candidate selectedSecond = null;
-              while(selectedFirst == null || selectedSecond == null){
-                double rouletteBall = ThreadLocalRandom.current().nextDouble(1);
-                for(int i = 0; i < populationSize; i++){
-                  if((i > 0 && (rouletteBall < temp.get(i).rouletteSlice && rouletteBall > temp.get(i - 1).rouletteSlice)) || (i < 0 && (rouletteBall < temp.get(i).rouletteSlice))) {
-                    if(selectedFirst == null){
-                      selectedFirst = temp.get(i);
-                    }else{
-                      selectedSecond = temp.get(i);
-                    }
-                  }
-                }
-              }
+									if(Collections.frequency(usedBitstrings, newBitstring1) < 200){ // Limit a single bitstring to 200 members of the next generation, to encourage diversity.
+										population.add(new Candidate(newBitstring1));
+										usedBitstrings.add(newBitstring1);
+									}
+									if(Collections.frequency(usedBitstrings, newBitstring2) < 200){
+										population.add(new Candidate(newBitstring2));
+										usedBitstrings.add(newBitstring2);
+									}
 
-              String newBitstring = "";
+	              }else{
 
-              if(ThreadLocalRandom.current().nextDouble(1) > candidateMutationProbability){
+	                // Begin mutation of child candidate
 
-                // Begin crossover of selected candidates to create child candidate
+	                String bitStringCopy = "";
+	                for(int i = 0; i < selectedFirst.bitstring.length(); i++){
+	                  if(ThreadLocalRandom.current().nextDouble(1) < bitMutationRate){
+	                    bitStringCopy += selectedFirst.bitstring.charAt(i) == '0' ? '1' : '0';
+	                  }else{
+	                    bitStringCopy += selectedFirst.bitstring.charAt(i);
+	                  }
+	                }
+	                String newBitstring = bitStringCopy;
 
-                int slicePoint = ThreadLocalRandom.current().nextInt(1, bitLength); //Crossover at least 1 char at the very beginning or very end, never before or after the entire bitstring
-                newBitstring = selectedFirst.bitstring.substring(0, slicePoint) + selectedSecond.bitstring.substring(slicePoint);
+									if(Collections.frequency(usedBitstrings, newBitstring) < 200){ // Limit a single bitstring to 200 members of the next generation, to encourage diversity.
+										population.add(new Candidate(newBitstring));
+										usedBitstrings.add(newBitstring);
+									}
 
-              }else{
+	              }
 
-                // Begin mutation of child candidate (possibly)
+	          }
 
-                String bitStringCopy = "";
-                for(int i = 0; i < selectedFirst.bitstring.length(); i++){
-                  if(ThreadLocalRandom.current().nextDouble(1) < bitMutationRate){
-                    bitStringCopy += selectedFirst.bitstring.charAt(i) == '0' ? '1' : '0';
-                  }else{
-                    bitStringCopy += selectedFirst.bitstring.charAt(i);
-                  }
-                }
-                newBitstring = bitStringCopy;
+	          probabilitySum = 0;
+	          fitnessSum = 0;
 
-              }
+	        }
 
-              // Newly spawned child candidate becomes a functioning member of society.
+	      }
 
-              population.add(new Candidate(newBitstring));
+	    }
+	    catch(Exception e){
+        System.out.println("There was a problem writing output data. Exiting.");
+        System.exit(0);
+	    }
 
-          }
+	    // Let's actually figure out what that bitstring represents.
 
-          probabilitySum = 0;
-          fitnessSum = 0;
+	    System.out.println("\nPerfect candidate " + perfectCandidate.bitstring + " found in generation " + generation + " with a fitness of " + perfectCandidate.fitness + " yields LCS '" + perfectCandidate.toString(testStringA.length() < testStringB.length() ? testStringA : testStringB) + "'.\n");
 
-          // Higher bitstring lengths were significantly improved by reducing mutation rate if fitness begins to plateau.
-
-          if(generation > 5){
-            if(Math.abs(generationHistory.get(generation - 5) - generationHistory.get(generation - 1)) < 0.5 && bitMutationRate > 0.01){
-              System.out.println("Below target fitness improvement... Decreasing probability of mutation.");
-              bitMutationRate /= 2;
-              candidateMutationProbability /= 2;
-            }
-          }
-
-        }
-
-      }
-
-    }
-    catch(Exception e){
-
-    }
-
-    // Let's actually figure out what that bitstring represents.
-
-    String LCS = "";
-
-    for(int i = 0; i < perfectCandidate.bitstring.length(); i++){
-      if(perfectCandidate.bitstring.charAt(i) == '1'){
-        LCS += testStringA.charAt(i);
-      }
-    }
-
-    System.out.println("\nPerfect candidate " + perfectCandidate.bitstring + " found in generation " + generation + " with a fitness of " + perfectCandidate.fitness + " yields LCS '" + LCS + "'.\n");
-
+		}while(choice != '5');
   }
 
 }
